3d852dc059fee71fedcbdc7e36ab206d594ab910
Evan Scott <evan.scott@dc.rr.com>
2025-07-24 20:04:45 -0700

    implement cosine similarity search on chunks based on query

--- utils/answer_retrieval.py
+++ utils/answer_retrieval.py
@@ -1,5 +1,34 @@
-def retrieve_content():
-    pass
+import numpy as np
+from embedding_handler import compute_embeddings
+from config import config
+from typing import Dict, List, Tuple
 
-def cosine_similarity():
-    pass
+def retrieve_top_k_scores(
+        query:str,
+        mapped_document_db:Dict[str, Dict[str, List[float]]]
+        ) -> List[Tuple[str, str, float]]:
+    """
+    
+    """
+    query_embeddings = np.array(compute_embeddings(query, config.tokenizer, config.embedding_model))
+    query_embeddings = normalize_vector(query_embeddings)
+
+    scores = []
+
+    for doc_id, document_chunks in mapped_document_db.items():
+        for chunk_id, chunk_embeddings in document_chunks.items():
+
+            chunk_embeddings = np.array(chunk_embeddings)
+            chunk_embeddings = normalize_vector(chunk_embeddings)
+            cosine_similarity_score = np.dot(query_embeddings, chunk_embeddings)
+            scores.append(doc_id, chunk_id, cosine_similarity_score)
+
+    scores.sort(key=lambda x: x[2], reverse=True)
+
+    return scores[:config.top_k]
+
+def normalize_vector(vector: np.array) -> float:
+    norm = np.linalg.norm(vector)
+    if norm == 0:
+        return vector
+    return vector / norm
