2e237a3f00deb5a410b75bc217f02d10f23f91d5
Evan Scott <evan.scott@dc.rr.com>
2025-07-28 19:19:39 -0700

    increase similarity score lower bound to avoid neutral data

--- utils/answer_retrieval.py
+++ utils/answer_retrieval.py
@@ -22,7 +22,7 @@ def retrieve_top_k_scores(
             chunk_embeddings = normalize_vector(chunk_embeddings)
             cosine_similarity_score = np.dot(query_embeddings, chunk_embeddings)
             
-            if cosine_similarity_score >= 0.5:
+            if cosine_similarity_score >= 0.6:
                 scores.append((doc_id, chunk_id, cosine_similarity_score))
 
     scores.sort(key=lambda x: x[2], reverse=True)
