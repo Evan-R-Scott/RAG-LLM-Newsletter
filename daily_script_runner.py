f9a4b3a083a050383742408ac4f24994bd943c68
Evan Scott <evan.scott@dc.rr.com>
2025-07-28 19:19:11 -0700

    fix comment and order of funcs calls

--- daily_script_runner.py
+++ daily_script_runner.py
@@ -5,11 +5,12 @@ from utils.data_io import save_json
 from document_fetch.newsletter_data_fetch import parse_feeds
 
 def run():
+    # saving these is actually unnecessary extra storage but done for debugging/expansion purposes 
     parse_feeds()
     documents_data = chunk_files(config.document_directory)
     save_json(config.document_store, documents_data)
-    mapped_document_db = map_embeddings(documents_data)
-    save_json(config.embedding_store, mapped_document_db)
+    documents_embeddings = map_embeddings(documents_data)
+    save_json(config.embedding_store, documents_embeddings)
     
 if __name__ == "__main__":
     run()
