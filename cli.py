9efb4fbd43add523979697aab4fc3db8bac98314
Evan Scott <evan.scott@dc.rr.com>
2025-07-28 19:18:17 -0700

    fail case when no newsletter data matched the user's query

--- cli.py
+++ cli.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from settings.app_config import config
 from utils.answer_retrieval import retrieve_top_k_scores
@@ -11,15 +11,15 @@ from utils.llm import generate_llm_response
 
 app = FastAPI(title="Evan's Chatbot")
 
+class Message(BaseModel):
+    message: str
+
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 def chatbot(request: Request):
     return templates.TemplateResponse("chat.html", {"request": request})
-
-class Message(BaseModel):
-    message: str
 
 @app.post("/chat", response_class=JSONResponse)
 def chat_endpoint(query: Message):
@@ -28,11 +28,15 @@ def chat_endpoint(query: Message):
     documents_embeddings = read_json(config.embedding_store)
 
     top_k_similars = retrieve_top_k_scores(message, documents_embeddings)
-    text_related, json_related = retrieve_text(top_k_similars, documents_data)
-    #save_json("random.json", json_related)
-    llm_summary = generate_llm_response(text_related)
+    
+    if len(top_k_similars) == 0:
+        text_related = "No newsletter data was found related to your query."
+        json_related = ["No newsletter data was found related to your query."]
+    else:
+        text_related, json_related = retrieve_text(top_k_similars, documents_data)
+        text_related = generate_llm_response(text_related)
 
     return {
-        "summary": llm_summary,
+        "summary": text_related,
         "related_text": json_related
     }
