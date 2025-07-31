c52e4faaeb4b4746d1a6b863e3aea9e526dc16bd
Evan Scott <evan.scott@dc.rr.com>
2025-07-30 22:14:24 -0700

    converted Config class holding hyperparameters for RAG pipeline into a Singleton pattern for consistent params

--- settings/app_config.py
+++ settings/app_config.py
@@ -1,33 +1,38 @@
 import json
-from dataclasses import dataclass
-from typing import List
+from typing import Optional
 from transformers import AutoTokenizer, AutoModel
 
-@dataclass
+
+""" cls refers to the class itself bc of decorator whereas self refers to the instance"""
 class Config:
-    document_directory: str
-    rss_feeds_store: str
-    document_store: str
-    embedding_store: str
-    tokenizer: str
-    embedding_model: str
-    top_k: int
-    chunk_size: int
-    supported_extensions: List[str]
-    para_separator: str
-    separator: str
+    # TODO do i want this to be a singleton? bc what happens if i update config but it keeps getting old settings, when would i know to reset it or just have it create each time so i can guarantee its up to date
+    """Singleton"""
+    _instance: Optional['Config'] = None
+    _initialized: bool = False
 
+    def __init__(self):
+        if not Config._initialized:
+            with open('settings/config.json', 'r') as f:
+                data = json.load(f)
+            
+            for key, value in data.items():
+                setattr(self, key, value)
+
+            self.tokenizer = AutoTokenizer.from_pretrained(self.tokenizer)
+            self.embedding_model = AutoModel.from_pretrained(self.embedding_model)
+            self.embedding_model.eval()
+            Config._initialized = True
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+    
     @classmethod
-    def from_json(cls, config_path: str = 'settings/config.json'):
-        with open(config_path, 'r') as f:
-            data = json.load(f)
-        return cls(**data)
+    def get_instance(cls):
+        if cls._instance is None:
+            cls._instance = cls()
+        return cls._instance
 
-config = Config.from_json()
-
-config.tokenizer = AutoTokenizer.from_pretrained(config.tokenizer)
-config.embedding_model = AutoModel.from_pretrained(config.embedding_model)
-config.embedding_model.eval()
-
-
-# add logger
+# initialize Singleton for later use
+config = Config.get_instance()
