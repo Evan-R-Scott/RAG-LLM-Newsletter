8c162dc3a785747fde803c007a127978cbd88363
Evan Scott <evan.scott@dc.rr.com>
2025-07-30 22:17:58 -0700

    incorporate new Singleton pattern for Config and Logging objects for llm summarization

--- utils/llm.py
+++ utils/llm.py
@@ -1,2 +1,7 @@
+from settings import Config, Logger
+
+config = Config.get_instance()
+logger = Logger.get_runtime_logger("chatbot")
+
 def generate_llm_response(text_related:str):
     return text_related
