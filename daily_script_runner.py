ca07be9df322ff100e9c425844ff8ffcd70dc44f
Evan Scott <evan.scott@dc.rr.com>
2025-07-30 22:11:43 -0700

    implement chunking and embedding into one process for efficiency with storage

--- daily_script_runner.py
+++ daily_script_runner.py
@@ -1,16 +1,19 @@
-from settings.app_config import config
+from settings import Config, Logger
 from utils.document_parser import chunk_files
-from utils.embedding_handler import map_embeddings
-from utils.data_io import save_json
 from document_fetch.newsletter_data_fetch import parse_feeds
+
+config = Config.get_instance()
+logger = Logger.get_daily_logger("data_fetch")
 
 def run():
     # saving these is actually unnecessary extra storage but done for debugging/expansion purposes 
     parse_feeds()
-    documents_data = chunk_files(config.document_directory)
-    save_json(config.document_store, documents_data)
-    documents_embeddings = map_embeddings(documents_data)
-    save_json(config.embedding_store, documents_embeddings)
+    chunk_files(config.document_directory)
+    # save_json(config.document_store, documents_data)
+    # documents_embeddings = map_embeddings(documents_data)
+    # save_json(config.embedding_store, documents_embeddings)
+
+    # TODO: store these as fields of some class here and inside some data structure so can import and use at runtime for chatbot? instead of having to read each out every time a query is processed
     
 if __name__ == "__main__":
     run()
