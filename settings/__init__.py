b30b26042f5cb72b1f3ae8638bd820bed8fcd3e0
Evan Scott <evan.scott@dc.rr.com>
2025-07-31 15:36:08 -0700

    package class dependencies into the settings folder for easy universal imports

--- settings/__init__.py
+++ settings/__init__.py
@@ -1,3 +1,3 @@
-from app_config import Config
-from app_logger import Logger
-from vector_store import VectorStore, Chunk
+from .app_config import Config
+from .app_logger import Logger
+from .vector_store import VectorStore, Chunk
