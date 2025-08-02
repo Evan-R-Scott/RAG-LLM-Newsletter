c756fc4ab32fd936de719d00084988c02cfbd369
Evan Scott <evan.scott@dc.rr.com>
2025-08-01 17:14:02 -0700

    add logging and correct func calls in RAG pipeline at runtime

--- cli.py
+++ cli.py
@@ -7,6 +7,7 @@ from pydantic import BaseModel, Field
 from settings import Config, Logger, VectorStore
 from utils.llm import generate_llm_response
 from utils.embedding_handler import prepare_embeddings
+from utils.data_io import format_chunks
 
 app = FastAPI(title="Evan's Chatbot")
 config = Config.get_instance()
@@ -31,18 +32,18 @@ def chat_endpoint(query: Message):
     runtime_logger.info(f"Query submitted at /chat endpoint: {message}")
     query_embedding = prepare_embeddings(message)
 
-    related_chunk_objs = vector_store.retrieve_top_k(query_embedding=query_embedding)
+    # chunk objecs of highest similarity to query (up to top_k articles)
+    related_articles = vector_store.retrieve_top_k(query_embedding=query_embedding)
 
-    if len(related_chunk_objs) == 0:
-        runtime_logger.info(f"There was no article with content related to the user's query: {query}")
-        text_related = "No newsletter data was found related to your query."
+    if len(related_articles) == 0:
+        runtime_logger.info(f"There were no articles with content relevant to the user's query: {query}")
+        articles_list = "No newsletter data was found related to your query."
         json_formatted = ["No newsletter data was found related to your query."]
     else:
-        runtime_logger.info(f"Found {len(text_related)} articles of relative similarity to user's query: {query}")
-        text_related, json_formatted = format(related_chunk_objs)
-        text_related = generate_llm_response(text_related)
-
+        runtime_logger.info(f"Found {len(related_articles)} articles of relative similarity to user's query: {query}")
+        articles_list, json_formatted = format_chunks(related_articles)
+        articles_list = generate_llm_response(message, articles_list)
     return {
-        "summary": text_related,
+        "summary": articles_list,
         "related_text": json_formatted
     }
--- daily_script_runner.py
+++ daily_script_runner.py
@@ -1,22 +1,15 @@
-from settings import Config, Logger
+from settings import Config, Logger, VectorStore
 from utils.document_parser import chunk_files
 from document_fetch.newsletter_data_fetch import parse_feeds
-from utils.data_io import save_pkl
 
 config = Config.get_instance()
 logger = Logger.get_daily_logger("data_fetch")
 
 def run():
-    # saving these is actually unnecessary extra storage but done for debugging/expansion purposes 
     parse_feeds()
     vector_store = chunk_files(config.document_directory)
     # write out to a .pkl file for later use at runtime by chatbot
     vector_store.save()
-    # save_json(config.document_store, documents_data)
-    # documents_embeddings = map_embeddings(documents_data)
-    # save_json(config.embedding_store, documents_embeddings)
-
-    # TODO: store these as fields of some class here and inside some data structure so can import and use at runtime for chatbot? instead of having to read each out every time a query is processed
     
 if __name__ == "__main__":
     run()
